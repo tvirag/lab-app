0004b8cb048319fbdb908595e2948efb9fd7c988
Tamas Virag <tamas.virag@gov.ab.ca>
2018-01-09 15:38:21 -0700

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  posts: any[];
+  constructor(private http: HttpClient) {
+	this.http.get('http://localhost:3000/posts').subscribe(t=> {
+		console.log(t);
+		this.posts = t;
+	});
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AlertModule.forRoot(),
+    BrowserModule,
+	BsDropdownModule.forRoot(),
+	HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
