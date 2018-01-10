824882b0b8fc1a84d8747ccde1e43c15164a693a
Tamas Virag <tamas.virag@gov.ab.ca>
2018-01-10 10:37:30 -0700

    after fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,9 @@ export class AppComponent {
   title = 'app';
   posts: any[];
   constructor(private http: HttpClient) {
-	this.http.get('http://localhost:3000/posts').subscribe(t=> {
+	this.http.get<any[]>('http://localhost:3000/posts').subscribe(t=> {
 		console.log(t);
 		this.posts = t;
 	});
-  }
-
-
+}
 }
